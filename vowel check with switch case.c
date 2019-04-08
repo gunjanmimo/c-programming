12e1c20d25cfcdef7412afaf2cff005fe2f07f6a
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2019-04-08 22:30:07 +0530

    Create vowel check with switch case.c

---
+++ vowel check with switch case.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+void main(){
+  char c;
+  scanf("%c",&c);
+  switch (c) {
+    case 'a':
+    printf("vowel");
+    break;
+    case 'e':
+    printf("vowel");
+    break;
+    case 'i':
+    printf("vowel");
+    break;
+    case 'o':
+    printf("vowel");
+    break;
+    case 'u':
+    printf("vowel");
+    break;
+    case 'A':
+    printf("vowel");
+    break;
+    case 'E':
+    printf("vowel");
+    break;
+    case 'I':
+    printf("vowel");
+    break;
+    case 'O':
+    printf("vowel");
+    break;
+    case 'U':
+    printf("vowel");
+    break;
+
+    default:
+    printf("not VOWEL");
+  }
+}
